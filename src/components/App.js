917e3d46a357542a229f4ed40d7374d75f8bdf71
Kat Hicks <katherine_m_hicks@hotmail.com>
2018-01-18 20:10:16 +0000

    Extract about, footer and navigation components

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import Menu from './Navigations/Menu'
 import Header from './Header'
+import About from './About'
+import Footer from './Footer'
+import Navigation from './Navigation'
 
 import filters from '../mocks/filters'
 import books from '../mocks/books'
@@ -49,23 +52,9 @@ class App extends React.Component {
           toggleMenu={ this.toggleMenu }
         />
 
-        <nav className="navbar navbar-default navbar-fixed-top navbar-custom">
-            <div className="container">
-                <div className="navbar-header">
-                    <a className="navbar-brand">LeanJS</a>
-                </div>
-                <ul className="nav navbar-nav pull-right">
-                    <li className="hidden-xs">
-                        <a href="#about">About us</a>
-                    </li>
-                    <li>
-                      <button onClick={ this.toggleMenu } className="btn btn-lg btn-outline">
-                        <i className="fa fa-graduation-cap"></i> <span>Training</span>
-                      </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <Navigation
+          toggleMenu={ this.toggleMenu }
+        />
 
         <Header title="Library" />
 
@@ -96,78 +85,10 @@ class App extends React.Component {
           </div>
         </section>
 
-        <section id="about" className="success">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12 text-center">
-                        <h2>About The Library</h2>
-                        <hr className="star-light" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-4 col-lg-offset-2">
-                        <p>This library is an exercise for building UI in a <strong style={{textDecoration: 'underline'}}>declarative way</strong>. This web site will help you understand the fundamental piece of ReactJS, components. You'll learn how to break an app in components (AKA componentization) and flow data accross them.</p>
-                    </div>
-                    <div className="col-lg-4">
-                        <p>This ReactJS web site works, but it's not well implemented. The problem, the developer didn't think in React and there aren't many components. We challenge you to fork the repo an componentizise the app to unleash the power of ReactJS.</p>
-                    </div>
-                    <div className="col-lg-8 col-lg-offset-2 text-center">
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/leanjscom/thinking-in-react" className="btn btn-lg btn-outline">
-                            <i className="fa fa-code-fork"></i> Fork me on Github
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <About/>
 
-        <footer className="text-center">
-            <div className="footer-above">
-                <div className="container">
-                    <div className="row">
-                        <div className="footer-col col-md-4">
-                            <h3>Main Location</h3>
-                            <p>
-                              <span>1 Fore Stree</span>
-                              <br />
-                              <span>London, EC2Y 5EJ</span>
-                            </p>
-                        </div>
-                        <div className="footer-col col-md-4">
-                            <h3>Around the Web</h3>
-                            <ul className="list-inline">
-                                <li>
-                                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/leanjscom" className="btn-social btn-outline"><i className="fa fa-fw fa-github"></i></a>
-                                </li>
-                                <li>
-                                    <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/leanjscom" className="btn-social btn-outline"><i className="fa fa-fw fa-twitter"></i></a>
-                                </li>
-                                <li>
-                                    <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/leanjscom/" className="btn-social btn-outline"><i className="fa fa-fw fa-instagram"></i></a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="footer-col col-md-4">
-                            <h3>About LeanJS</h3>
-                            <p>
-                              <a href="https://leanjs.com/">LeanJS </a>
-                              <span>
-                                is a boutique of excellence focused on Lean, JavaScript, UX, Agile, and people.
-                              </span>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="footer-below">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <span>Copyright &copy;</span> <a href="https://leanjs.com/" target="_blank" rel="noopener noreferrer">LeanJS</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <Footer/>
+
       </div>
     )
   }
